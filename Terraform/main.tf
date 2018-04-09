31d08179d4e864566e7d992c55620fcd2b3f45b0
[Rommel93] <[rommel_juarez@epam.com]>
2018-04-08 21:56:13 -0500

    Starting configuration

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,20 +2,48 @@ provider "aws" {
 	region     = "us-east-1"
 }
 
+#Variables
+variable "port" {
+  description = "The port the server will use"
+  default = 80
+}
+variable "protocol" {
+  description = "The protocol used"
+  default = HTTP
+}
+
+#Resources
+#VPC
 resource "aws_vpc" "vpc_export" {
+
+
 tags {
 	Name = "Vpc-Export"
 }
 
 }
 
+#Route
+resource "aws_route" "route_export" {
+
+
+tags {
+	Name = "Route-Export"
+}
+
+}
+
+#Subnet
 resource "aws_subnet" "subnet_export" {
+
+
 tags {
 	Name = "Subnet-Export"
 }
 
 }
 
+#App Server
 resource "aws_instance" "app_server" {
         ami = "ami-53b7152e"
         instance_type = "t2.micro"
@@ -26,7 +54,7 @@ resource "aws_instance" "app_server" {
  }
 }
 
-
+#DB Server
 resource "aws_db_instance" "db_server" {
 
  tags {
@@ -34,6 +62,7 @@ resource "aws_db_instance" "db_server" {
  }
 }
 
+#Launch Configuration
 resource "aws_launch_configuration" "web_launch_conf" {
   image_id      = ""
   instance_type = ""
@@ -44,6 +73,7 @@ tags {
 
 	}
 
+#Auto Scaling Group
 	resource "aws_autoscaling_group" "webserver_asg" {
   name                 = "terraform-asg-example"
   launch_configuration = "${aws_launch_configuration.web_launch_conf.name}"
@@ -54,14 +84,30 @@ tags {
 
 }
 
+#Load Balancer
+resource "aws_lb" "webserver" {
+  # ...
+}
+
+resource "aws_lb_target_group" "webserver" {
+		name     = "tf-example-lb-tg"
+		port     = "${var.port}"
+		protocol = "${var.protocol}"
+		vpc_id   = "${aws_vpc.vpc_export.id}"
+}
+
 resource "aws_lb_listener" "webserver_elb" {
+		load_balancer_arn = "${aws_lb.webserver.arn}"
+		port = "${var.port}"
+		protocol = "${var.protocol}"
 
  tags {
 		Name = "Elb-Export"
 	}
 
 }
-
+#Security Groups
+#Webserver Security Group
 resource "aws_security_group" "webserver_group" {
 
 tags {
@@ -69,6 +115,7 @@ tags {
 	}
 }
 
+#App Server Security Group
 resource "aws_security_group" "appserver_group" {
 
 tags {
@@ -76,6 +123,7 @@ tags {
 	}
 }
 
+#DB Security Group
 resource "aws_security_group" "dbserver_group" {
 
 tags {
