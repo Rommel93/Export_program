95e0e551b07535bc1bc5a28d524058c3188957af
Rommel Juarez <rommel_juarez@epam.com>
2018-04-30 17:09:23 -0500

    adding Chef practice

---
+++ Chef/apache_cookbook/metadata.rb
@@ -0,0 +1,9 @@
+name 'apache_cookbook'
+maintainer 'Rommel'
+maintainer_email 'rommel_juarez@epam.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
+issues_url 'https://github.com/learn-chef/learn_chef_apache2/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/learn_chef_apache2' if respond_to?(:source_url)
